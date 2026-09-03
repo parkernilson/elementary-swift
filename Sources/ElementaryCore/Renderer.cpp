b98c20bdbae8c2331582801442174e13e0391fc8
Parker Nilson <p_nilson@apple.com>
2026-09-03 15:28:49 -0700

    add a comment to Renderer

--- Sources/ElementaryCore/Renderer.cpp
+++ Sources/ElementaryCore/Renderer.cpp
@@ -3,6 +3,16 @@
 
 namespace ElementaryCore {
 
+/**
+ * Since ElementaryCore::Runtime and ElementaryCore::Renderer are just a lightweight shim layer
+ * around the underlying elem::Runtime<FloatType> and elem::Renderer<FloatType> so that we can expose
+ * their methods and constructors with Swift friendly types.
+ * 
+ * We use friend class status to grab a shared_ptr to `runtime.mRuntime` So that we can wire
+ * up the underlying `elem::Renderer` directly to `elem::Runtime`.
+ * 
+ * FloatType is hardcoded to float since AVAudioEngine uses Float32.
+ */
 Renderer::Renderer(const Runtime& runtime)
     : mRenderer{std::make_unique<elem::Renderer<float>>(runtime.mRuntime)} {}
 
@@ -21,7 +31,7 @@ void Renderer::renderGraph(NodeReprSPtrVector graphs,
                            elem::RenderOptions options) {
     mRenderer->renderGraph(std::move(graphs), std::move(options));
     
-    // TODO: Should we return RenderStats?
+    // TODO: Return RenderStats
 }
 
 } // namespace ElementaryCore
