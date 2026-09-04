6160ad75ad4e24cc93cd413ce0f5a02d5ad90957
Parker Nilson <p_nilson@apple.com>
2026-09-03 17:05:53 -0700

    clean up Runtime.swift

--- Sources/Elementary/Runtime.swift
+++ Sources/Elementary/Runtime.swift
@@ -6,11 +6,7 @@ public final class Runtime {
     public init(sampleRate: Double, blockSize: Int32) {
         coreRuntime = elemswift.Runtime(sampleRate, blockSize)
     }
-
     
-    // TODO: Callers can only call this if they import ElementaryCore to be able
-    // to have an elemswift.Runtime to call it with, so I think because of that
-    // it's okay to have this in the public interface
     /**
      * Construct this runtime from a custom runtime.
      * This can be used to set up the runtime with c++ methods like
@@ -21,9 +17,6 @@ public final class Runtime {
     }
 
     /// Processes one block of audio in place, using non-interleaved buffers.
-    /// Real-time safe: forwards directly to the underlying C++ call with no
-    /// allocation, so this is fine to call from a live audio render callback.
-    /// TODO: Verify this works correctly and is safe
     public func process(
         outputChannelData: UnsafeMutablePointer<UnsafeMutablePointer<Float>?>,
         numChannels: Int,
